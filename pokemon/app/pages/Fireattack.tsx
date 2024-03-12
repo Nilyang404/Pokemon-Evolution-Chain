6152f3f8fec316cfd208fdc5a372ce71692304f1
nilyang404 <nilyangcode@gmail.com>
2024-03-12 12:01:15 +1100

    add todo

---
+++ pokemon/app/pages/Fireattack.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useEffect, useState } from "react";
+//TODO:
+
+/*
+https://pokeapi.co/api/v2/generation/{id or name}/
+
+name:
+url:https://pokeapi.co/api/v2/region/1/
+-> 
+species:
+artr
+sort
+display
+*/
